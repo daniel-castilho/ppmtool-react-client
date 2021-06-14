70da251a7fc72b659ebddf72affafda718aa7008
Daniel Castilho <dan.castilho@gmail.com>
2021-06-14 13:48:24 -0400

    Project and Header components

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+import Header from "./Leiaute/Header";
 
 class Dashboard extends Component {
 	render() {
 		return (
-			<div>
+			<div className="">
 				<h1 className="mr-1">Dashboard</h1>
+				<Header />
+				<ProjectItem />
 			</div>
 		);
 	}
