71ae2a94a25200f182503483aae4e5bcb7d43c1f
Daniel Castilho <dan.castilho@gmail.com>
2021-06-14 14:41:06 -0400

    React Router first Functional component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Leiaute/Header";
+import AddProject from "./components/Project/AddProject";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./App.css";
+
 
 function App() {
 	return (
-		<div className="App">
-			<Header />
-			<Dashboard />
-		</div>
+		<Router>
+			<div className="App">
+				<Header />
+				<Route exact path="/dashboard" component={Dashboard} />
+				<Route exact path="/addProject" component={AddProject} />
+			</div>
+		</Router>
 	);
 }
 
